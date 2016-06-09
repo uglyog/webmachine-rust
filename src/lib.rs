79643f0e66757f831e4097ca0316342fdf8d2150
Ronald Holshausen <rholshausen@dius.com.au>
2016-06-09 22:12:28 +1000

    add the basic structs and support libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+//! The `webmachine-rust` crate provides a port of webmachine to rust.
+
+#![warn(missing_docs)]
+
+extern crate hyper;
+#[macro_use] extern crate log;
+#[macro_use] extern crate p_macro;
+#[macro_use] extern crate maplit;
+
+use std::collections::BTreeMap;
+use hyper::server::{Request, Response};
+
+/// Struct to represent a resource in webmachine
+pub struct WebmachineResource {
+
+}
+
+/// The main hyper dispatcher
+pub struct WebmachineDispatcher {
+    routes: BTreeMap<String, WebmachineResource>
+}
+
+impl WebmachineDispatcher {
+    /// Main hyper dispatch function for the Webmachine
+    pub fn dispatch(mut req: Request, mut res: Response) {
+
     }
 }
+
+#[cfg(test)]
+mod tests;
